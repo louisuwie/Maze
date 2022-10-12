fbdba35cdc5883b8e2b25284ffc3395dd88d06df
louisuwie <115404931+louisuwie@users.noreply.github.com>
2022-10-12 13:01:41 +0800

    Update maze.py

--- maze.py
+++ maze.py
@@ -1,19 +1,3 @@
-# LOUIS G BINWAG III
-# 200747
-# OCTOBER 2, 2022
-
-# I have not discussed the Python language code in my program 
-# with anyone other than my instructor or the teaching assistants 
-# assigned to this course.
-
-# I have not used Python language code obtained from another student, 
-# or any other unauthorized source, either modified or unmodified.
-
-# If any Python language code or documentation used in my program 
-# was obtained from another source, such as a textbook or website, 
-# that has been clearly noted with a proper citation in the comments 
-# of my program.
-
 # --------------------
 # Function Definitions
 # --------------------
